55ba5864daeb697ff172ea914a4d26c15e4bd97e
greg <greigdillon@gmail.com>
2022-02-07 06:37:40 -0400

    Added test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
 #include "gtest/gtest.h"
+#include "objectbox/objectbox.h"
+#include "absl/memory/memory.h"
 
 TEST(Tesing,Libraries){
+  std::cout << obx_version_string() << std::endl;
 
-  EXPECT_EQ(1, 1);
+  auto i = absl::make_unique<int>(9);
+  std::cout << *i.get() << std::endl;
+
+  EXPECT_EQ(3, 1);
 }
