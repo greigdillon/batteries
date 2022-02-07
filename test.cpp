335b78ef22fa1bb7514c700a5a74cd8dfff7216e
greg <greigdillon@gmail.com>
2022-02-07 13:03:26 -0400

    Added boost and eigen versions

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include "include/Eigen/src/Core/util/Macros.h"
 #include "include/absl/base/config.h"
 #include "include/absl/strings/substitute.h"
 #include "gtest/gtest.h"
@@ -6,6 +7,8 @@
 #include "protobuf/port_def.inc"
 #include "flatbuffers/flatbuffers.h"
 #include "objectbox/objectbox.h"
+#include "boost/version.hpp"
+#include "Eigen/Eigen"
 
 TEST(Batteries, VersionNumbers){
 
@@ -28,5 +31,12 @@ TEST(Batteries, VersionNumbers){
 
   std::cout << absl::Substitute("Object Box\n");
   std::cout << absl::Substitute("$0\n\n",obx_version_string());
+
+  std::cout << absl::Substitute("Boost\n");
+  std::cout << absl::Substitute("$0\n\n",BOOST_LIB_VERSION);
+
+  std::cout << absl::Substitute("Eigen\n");
+  std::cout << absl::Substitute("$0.$1.$2\n\n",EIGEN_WORLD_VERSION,EIGEN_MAJOR_VERSION,EIGEN_MINOR_VERSION);
+
 
 }
